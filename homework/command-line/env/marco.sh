c3369cf29fc57113922776a75bb1dab002dc91c9
pjh456 <147148383@qq.com>
2026-09-04 19:25:08 +0800

    feat: 完成 Command 环境变量 01 题

---
+++ homework/command-line/env/marco.sh
@@ -0,0 +1,10 @@
+marco() {
+	export MARCO_POLO_DIR="$(pwd)"
+}
+polo() {
+	if [ -z "MARCO_POLO_DIR" ]; then
+		echo "Error: Run 'marco' first!"
+		return 1
+	fi
+	cd $MARCO_POLO_DIR || echo "Error: Directory no longer exists!"
+}
