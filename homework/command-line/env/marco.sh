eb3f1989b57a7b6666c13740ea209ae463f9ff89
pjh456 <147148383@qq.com>
2026-09-05 16:38:54 +0800

    fix: 修复 Command 环境变量 01 题语法错误

--- homework/command-line/env/marco.sh
+++ homework/command-line/env/marco.sh
@@ -2,9 +2,9 @@ marco() {
 	export MARCO_POLO_DIR="$(pwd)"
 }
 polo() {
-	if [ -z "MARCO_POLO_DIR" ]; then
+	if [ -z "$MARCO_POLO_DIR" ]; then
 		echo "Error: Run 'marco' first!"
 		return 1
 	fi
-	cd $MARCO_POLO_DIR || echo "Error: Directory no longer exists!"
+	cd "$MARCO_POLO_DIR" || echo "Error: Directory no longer exists!"
 }
