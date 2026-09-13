774c733db60117c86a67ba9724eb9c69e67a19c1
pjh456 <147148383@qq.com>
2026-09-13 16:58:07 +0800

    feat: 完成 Command Aliases 02 题别名创建

--- homework/command-line/aliases/2.sh
+++ homework/command-line/aliases/2.sh
@@ -1,1 +1,12 @@
 history | awk '{$1="";print substr($0,2)}' | sort | uniq -c | sort -n | tail -n 10
+
+alias ll='ls -lah'
+alias la='ls -A'
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gd='git diff'
+alias grep='rg --color=auto'
+alias cat='bat --paging=never'
+alias py='python3'
